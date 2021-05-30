0dfab22c3b2be745f7e1636daeb458510fff7133
Ben Fortuna <bfortuna@odecee.com.au>
2021-05-30 12:48:16 +1000

    Added vcard serialization

---
+++ src/main/java/org/mnode/ical4j/json/jot/CardBuilder.java
@@ -0,0 +1,16 @@
+package org.mnode.ical4j.json.jot;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import net.fortuna.ical4j.vcard.Property;
+import net.fortuna.ical4j.vcard.VCard;
+
+public class CardBuilder extends AbstractJotBuilder<VCard> {
+
+    @Override
+    public JsonNode build() {
+        ObjectNode node = createObjectNode();
+        putIfNotNull("id", node, component.getProperty(Property.Id.UID));
+        return node;
+    }
+}
