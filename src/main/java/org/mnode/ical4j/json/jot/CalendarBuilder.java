aeb3b1a1f4bbf64299ac59239a6d23ab4c28bbce
Ben Fortuna <bfortuna@odecee.com.au>
2021-06-14 13:20:28 +1000

    Skip unused properties

--- src/main/java/org/mnode/ical4j/json/jot/CalendarBuilder.java
+++ src/main/java/org/mnode/ical4j/json/jot/CalendarBuilder.java
@@ -3,6 +3,7 @@ package org.mnode.ical4j.json.jot;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Property;
 
 public class CalendarBuilder extends AbstractJotBuilder<Calendar> {
 
@@ -11,7 +12,14 @@ public class CalendarBuilder extends AbstractJotBuilder<Calendar> {
         ObjectNode node = createObjectNode();
 //        putIfNotNull("uid", node, component.getProperty(Property.UID));
         component.getProperties().forEach(property -> {
-            node.put(property.getName().toLowerCase(), property.getValue());
+            switch (property.getName()) {
+                // skip properties that have no meaning in jot..
+                case Property.PRODID:
+                case Property.VERSION:
+                    break;
+                default:
+                    node.put(property.getName().toLowerCase(), property.getValue());
+            }
         });
         return node;
     }
