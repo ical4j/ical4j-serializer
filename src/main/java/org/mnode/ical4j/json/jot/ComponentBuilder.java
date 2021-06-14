123f3f55741a4a3820e3962f235ede8e000c848d
Ben Fortuna <bfortuna@odecee.com.au>
2021-06-14 13:20:03 +1000

    Added organizer property builder

--- src/main/java/org/mnode/ical4j/json/jot/ComponentBuilder.java
+++ src/main/java/org/mnode/ical4j/json/jot/ComponentBuilder.java
@@ -3,6 +3,8 @@ package org.mnode.ical4j.json.jot;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.property.Organizer;
 
 public class ComponentBuilder<T extends Component> extends AbstractJotBuilder<T> {
 
@@ -11,7 +13,23 @@ public class ComponentBuilder<T extends Component> extends AbstractJotBuilder<T>
         ObjectNode node = createObjectNode();
 //        putIfNotNull("uid", node, component.getProperty(Property.UID));
         component.getProperties().forEach(property -> {
-            node.put(property.getName().toLowerCase(), property.getValue());
+            switch (property.getName()) {
+                case Property.ORGANIZER:
+                    node.set(property.getName().toLowerCase(), new OrganizerBuilder()
+                            .component((Organizer) property).build());
+                    break;
+                case Property.LOCATION:
+                case Property.RESOURCES:
+                case Property.ATTACH:
+                case Property.RELATED_TO:
+                case Property.ATTENDEE:
+                case Property.TRIGGER:
+                case Property.COMMENT:
+                case Property.CONTACT:
+                case Property.FREEBUSY:
+                default:
+                    node.put(property.getName().toLowerCase(), property.getValue());
+            }
         });
         return node;
     }
