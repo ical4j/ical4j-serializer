b4fa014b7ccfae41ae66ce4f8253aa6549b73488
Ben Fortuna <fortuna@micronode.com>
2024-02-24 14:35:45 +1100

    Added Java module support

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module ical4j.serializer {
+    requires java.base;
+    requires ical4j.core;
+    requires ical4j.vcard;
+    requires ical4j.extensions;
+    requires ical4j.template;
+
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.xml;
+    requires org.apache.commons.codec;
+
+    exports org.mnode.ical4j.serializer;
+    exports org.mnode.ical4j.serializer.jotn;
+    exports org.mnode.ical4j.serializer.jotn.component;
+    exports org.mnode.ical4j.serializer.jotn.property;
+    exports org.mnode.ical4j.serializer.atom;
+    exports org.mnode.ical4j.serializer.jmap;
+    exports org.mnode.ical4j.serializer.jsonld;
+}
